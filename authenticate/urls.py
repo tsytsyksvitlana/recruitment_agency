363966859eab9b915e35c9d1a65cdfdb60d362a6
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2025-03-23 21:05:41 +0200

    added api for jwt auth

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-#     TokenVerifyView,
-# )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from authenticate.views import (
     UserLoginView,
@@ -17,15 +17,15 @@ urlpatterns = [
     path("auth/register/", UserRegisterView.as_view(), name="register"),
     path("auth/login/", UserLoginView.as_view(), name="login"),
     path("auth/logout/", UserLogoutView.as_view(), name="logout"),
-    # path(
-    #     "api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
-    # ),
-    # path(
-    #     "api/v1/token/refresh/",
-    #     TokenRefreshView.as_view(),
-    #     name="token_refresh",
-    # ),
-    # path(
-    #     "api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"
-    # ),
+    path(
+        "api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/v1/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        "api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"
+    ),
 ]
---
+++ exception_handlers/base.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+
+
+class BaseException(Exception):
+    status_code: int
+    message: str
+    detail_info: str | bool = False
+
+
+class NotRequiredData(Exception):
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    message = 'Not all necessary data provided'
+    detail_info = True
+
+
+class NoAuthUser(Exception):
+    status_code = status.HTTP_407_PROXY_AUTHENTICATION_REQUIRED
+    message = 'User have to be authenticated!'
+
+
+class EnoughtLikes(Exception):
+    status_code = status.HTTP_409_CONFLICT
+    message = 'You have already liked this post'
